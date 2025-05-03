de40b7344396c4ff49d72e72069510c052d5babe
Ritik Mitra <ritikmitra36@gmail.com>
2025-05-04 03:06:49 +0530

    fix: remove duplicate dotenv import and configuration in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 import https from 'https';
 import fs from 'fs';
 import { app } from './app';
-import dotenv from 'dotenv';
-dotenv.config({ path: './.env' });
 
 
 const PORT = process.env.PORT || 3000;
